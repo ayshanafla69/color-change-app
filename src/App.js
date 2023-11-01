ccfaf6ae154d1648c3bd3000f05f421dc1a78d38
ayshanafla69 <naflanafu64@gmail.com>
2023-11-01 17:58:19 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Button } from 'react-bootstrap';
+
+
+
+
+
 
 function App() {
+  
+ const [colorchange,setColorchange] = useState("")
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div  style={{height:'40vh'}}  className=' mt-5 d-flex justify-content-center align-items-center '>
+        <div className='text-center' >
+            
+          <h1 style={{fontSize:'50px',fontWeight:'revert'}}>Background-color-change</h1>
+          <div className='d-flex justify-content-between  align-items-center mt-5'></div>
+
+
+
+         <Button onClick={()=>setColorchange('red')} style={{color:'black' ,backgroundColor:'red',width:'100px'}} variant="outline-secondary" className='mx-2'>Red</Button>{' '}
+
+          <Button onClick={()=>setColorchange('Blue')} style={{color:'black' ,backgroundColor:'blue',
+          width:'100px'}} variant="outline-secondary" className='mx-2'>Blue</Button>{' '}
+          
+          <Button onClick={()=>setColorchange('yellow')} style={{color:'black' ,backgroundColor:'yellow',width:'100px'}} variant="outline-secondary" className='mx-2'>Yellow</Button>{' '}
+
+          <Button onClick={()=>setColorchange('pink')} style={{color:'black' ,backgroundColor:'pink',width:'100px'}} variant="outline-secondary" className='mx-2'>Pink</Button>{' '}
+
+          <a className='btn bg-black text-white' onClick={()=>setColorchange('')}>Reset</a>
+
+
+          </div>
+          
     </div>
+    <div  className='   d-flex justify-content-center align-items-center '>
+      <div style={{height:'300px',width:'500px',backgroundColor:`${colorchange}`}} className='border rounded-5 '>
+              <div style={{height:'300px'}} className='d-flex align-items-center justify-content-center'>
+                <h3 className='text-black'>{colorchange}</h3>
+              </div>
+      </div>
+
+
+    </div>
+    </>
   );
 }
 
